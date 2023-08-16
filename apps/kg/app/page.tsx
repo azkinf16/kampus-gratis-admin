9a59a7d93b2216a400b3e577e03921fc3802822c
Naza Zulfiqi <zulfiqinaza@gmail.com>
2023-08-16 17:46:07 +0700

    improvement:add image and icon landing page

--- apps/kg/app/page.tsx
+++ apps/kg/app/page.tsx
@@ -3,6 +3,8 @@ import styles from './page.module.css';
 export default async function Index() {
 
   return (
-    <></>
+    <>
+    <h1 className='text-3xl text-blue-600'>Hello World!</h1>
+    </>
   );
 }
