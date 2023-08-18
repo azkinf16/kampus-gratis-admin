0ca036aa958a616706bacdd62bf964c9f807a26c
imanuelcio <ciociocio009@gmail.com>
2023-08-18 15:29:25 +0700

    login-page: form page

--- apps/kg-admin/app/auth/login.tsx
+++ apps/kg-admin/app/auth/login.tsx
@@ -1,31 +1,50 @@
 import React from 'react';
+import Image from 'next/image';
 
 const login = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div
-        className="flex flex-col md:flex-row shadow-md rounded-md p-8 "
+        className="flex flex-col md:flex-row   rounded-md p-8 "
         style={{ backgroundColor: '#e9f6fd', width: '1080px' }}
       >
-        <div className="md:w-1/2 p-4">Gambar Disini</div>
-        <div className="md:w-1/2 p-4">
-          <h2 className="text-2xl font-semibold mb-4 text-center">Masuk</h2>
-          <h3 className="text-sm font-semibold mb-4">
+        <div className="p-4 relative flex justify-center md:w-1/2 ">
+          <div className="absolute left-5">
+            <Image
+              src={'./assets/ic-logo.svg'}
+              alt="logo"
+              width={100}
+              height={100}
+            />
+          </div>
+          <Image
+            src="./assets/ic-login.svg"
+            alt="logo"
+            width={253}
+            height={245}
+          />
+        </div>
+        <div className="md:w-1/2 md:p-7 p-4">
+          <h2 className="text-2xl font-bold mb-4 text-center tracking-wide">
+            Masuk
+          </h2>
+          <h3 className="text-[13px] font-semibold mb-4 text-center">
             Silahkan masuk menggunakan email dan kata sandi yang terdaftar
           </h3>
-          <form>
+          <form className="">
             <div className="mb-4">
               <label
-                htmlFor="username"
+                htmlFor="email"
                 className="block text-sm font-medium text-gray-700"
               >
-                Username
+                Email
               </label>
               <input
-                type="text"
-                id="username"
-                name="username"
+                type="email"
+                id="email"
+                name="email"
                 className="mt-1 p-2 w-full border rounded-md"
+                placeholder="Email"
               />
             </div>
             <div className="mb-4">
@@ -40,13 +59,20 @@ const login = () => {
                 id="password"
                 name="password"
                 className="mt-1 p-2 w-full border rounded-md"
+                placeholder="Password"
               />
+            </div>
+            <div className="flex justify-end pb-2 text-blue-900">
+              <a href="">
+                <p>Lupa Kata Sandi ?</p>
+              </a>
             </div>
             <button
               type="submit"
-              className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+              style={{ backgroundColor: '#0B568D' }}
+              className="text-white p-2 rounded-md hover:bg-blue-600 w-full"
             >
-              Login
+              Masuk
             </button>
           </form>
         </div>
