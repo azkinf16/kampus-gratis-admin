222524912c6ad9a746493e3ee450c130d9f3082f
azarnuzy <azarnuzy@gmail.com>
2023-08-21 19:41:36 +0700

    improvement: add env example

--- apps/kg/app/api/auth/[...nextauth]/route.ts
+++ apps/kg/app/api/auth/[...nextauth]/route.ts
@@ -7,12 +7,12 @@ import { TLoginData } from '../../../../types';
 import { loginByGoogleRequest, loginRequest } from '../../../../hooks';
 
 export const authOptions: AuthOptions = {
-  pages: {
-    signIn: '/auth/login',
-  },
-  session: {
-    strategy: 'jwt',
-  },
+  // pages: {
+  //   signIn: '/auth/login',
+  // },
+  // session: {
+  //   strategy: 'jwt',
+  // },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -24,97 +24,97 @@ export const authOptions: AuthOptions = {
         email: { label: 'Email', type: 'email' },
         password: { label: 'Password', type: 'password' },
       },
-      // async authorize(credentials): Promise<TLoginData> {
-      //   try {
-      //     const data = await loginRequest({
-      //       email: credentials?.email,
-      //       password: credentials?.password,
-      //     });
-      //     return data;
-      //   } catch (error: any) {
-      //     if (error.response.status === 422) {
-      //       throw new Error(error.response.data.message);
-      //     }
+      async authorize(credentials): Promise<TLoginData> {
+        try {
+          const data = await loginRequest({
+            email: credentials?.email,
+            password: credentials?.password,
+          });
+          return data;
+        } catch (error: any) {
+          if (error.response.status === 422) {
+            throw new Error(error.response.data.message);
+          }
 
-      //     throw new Error(
-      //       typeof error.response.data === 'string'
-      //         ? error.response.data
-      //         : error.response.data?.message
-      //     );
-      //   }
-      // },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null;
-
-        const data = await loginRequest({
-          email: credentials?.email,
-          password: credentials?.password,
-        });
-
-        if (!data) return null;
-
-        return data;
+          throw new Error(
+            typeof error.response.data === 'string'
+              ? error.response.data
+              : error.response.data?.message
+          );
+        }
       },
+      // async authorize(credentials) {
+      //   if (!credentials?.email || !credentials?.password) return null;
+
+      //   const data = await loginRequest({
+      //     email: credentials?.email,
+      //     password: credentials?.password,
+      //   });
+
+      //   if (!data) return null;
+
+      //   return data;
+      // },
     }),
   ],
-  // pages: {
-  //   signIn: '/auth/login',
-  //   signOut: '/auth/logout',
-  // },
-  // session: {
-  //   maxAge: 2 * 60 * 60,
-  // },
-  // callbacks: {
-  //   async signIn({ user, account }) {
-  //     if (account?.provider === 'google' && account) {
-  //       try {
-  //         const { data } = await loginByGoogleRequest({
-  //           credential: account.id_token as string,
-  //         });
-  //         account.access_token = data?.token?.access_token;
-  //         account.refresh_token = data?.token?.refresh_token;
-  //       } catch (error: any) {
-  //         return `/auth/login?error=${error.response.data?.message}`;
-  //       }
-  //     }
+  pages: {
+    signIn: '/auth/login',
+    signOut: '/auth/logout',
+  },
+  session: {
+    maxAge: 2 * 60 * 60,
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === 'google' && account) {
+        try {
+          const { data } = await loginByGoogleRequest({
+            credential: account.id_token as string,
+          });
+          account.access_token = data?.token?.access_token;
+          account.refresh_token = data?.token?.refresh_token;
+        } catch (error: any) {
+          return `/auth/login?error=${error.response.data?.message}`;
+        }
+      }
 
-  //     if (user) return true;
-  //     return false;
-  //   },
+      if (user) return true;
+      return false;
+    },
 
-  //   async jwt({ token, user, account }) {
-  //     const currentUser = user as unknown as TLoginData;
-  //     if (account?.provider === 'google' && account) {
-  //       token.access_token = account.access_token;
-  //       token.refresh_token = account.refresh_token;
-  //     } else if (account?.provider === 'login' && currentUser) {
-  //       token.access_token = currentUser.access_token;
-  //       token.refresh_token = currentUser.refresh_token;
-  //       currentUser.name = user.name;
-  //       currentUser.email = user.email;
-  //     }
+    async jwt({ token, user, account }) {
+      const currentUser = user as unknown as TLoginData;
+      if (account?.provider === 'google' && account) {
+        token.access_token = account.access_token;
+        token.refresh_token = account.refresh_token;
+      } else if (account?.provider === 'login' && currentUser) {
+        token.access_token = currentUser.access_token;
+        token.refresh_token = currentUser.refresh_token;
+        currentUser.name = user.name;
+        currentUser.email = user.email;
+      }
 
-  //     return { ...token, ...currentUser };
-  //   },
-  //   async session({ session, token }) {
-  //     console.log('session', session, token);
+      return { ...token, ...currentUser };
+    },
+    async session({ session, token }) {
+      console.log('session', session, token);
 
-  //     const jwt_token = {
-  //       access_token: token?.access_token,
-  //       refresh_token: token?.refresh_token,
-  //     };
-  //     session = {
-  //       expires: token?.expires as string,
-  //       user: {
-  //         id: 'id-user123456',
-  //         name: token.name,
-  //         email: token.email,
-  //         token: jwt_token,
-  //       },
-  //     };
-  //     return session;
-  //   },
-  // },
+      const jwt_token = {
+        access_token: token?.access_token,
+        refresh_token: token?.refresh_token,
+      };
+      session = {
+        expires: token?.expires as string,
+        user: {
+          id: 'id-user123456',
+          name: token.name,
+          email: token.email,
+          token: jwt_token,
+        },
+      };
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
