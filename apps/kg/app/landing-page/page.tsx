2388c0eec042e395d6af13958a33d3dd7e7e73a3
azarnuzy <azarnuzy@gmail.com>
2023-08-22 10:57:41 +0700

    improvement: fix node modules

--- apps/kg/app/landing-page/page.tsx
+++ apps/kg/app/landing-page/page.tsx
@@ -1,19 +1,13 @@
 'use client';
-
-import GlimpseSection from 'apps/kg/modules/landing/glimpse/section';
-import FeatureSection from 'apps/kg/modules/landing/feature/section';
-import HeroSection from 'apps/kg/modules/landing/hero/section';
 import { NextPage } from 'next';
-import { LandingPageModules } from 'apps/kg/modules/landing';
-
+import { LandingPageModules } from '../../modules';
 
 const LandingPage: NextPage = () => {
   return (
     <>
-    <LandingPageModules/>
+      <LandingPageModules />
     </>
   );
 };
 
 export default LandingPage;
-
