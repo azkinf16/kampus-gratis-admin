1adad4a8c2d58c20b7ee4570b4acbda39572852d
azkinf16 <azkifajri16@gmail.com>
2023-08-22 16:25:06 +0700

    improve: fix size layout dashboard

--- apps/kg-admin/components/jadwal-mk/Jadwalmk.tsx
+++ apps/kg-admin/components/jadwal-mk/Jadwalmk.tsx
@@ -6,15 +6,15 @@ export const Jadwalmk = () => {
     <div className="flex justify-between">
       <Image
         src="/assets/pic-jmk.png"
-        alt="pic" //altnya belum di fix
-        width={250}
-        height={250}
+        alt="pic-jmk" //altnya belum di fix
+        width={200}
+        height={200}
       />
       <div className="flex items-center ml-7">
-        <div className="flex flex-col gap-6">
-          <h1 className="font-bold text-xl">Webinar Cyber Security</h1>
-          <p className="text-lg">Selasa, 28 Februari 2023</p>
-          <p className="text-lg">Pukul 19:00 WIB</p>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-lg">Webinar Cyber Security</h1>
+          <p className="text-md">Selasa, 28 Februari 2023</p>
+          <p className="text-md">Pukul 19:00 WIB</p>
         </div>
       </div>
     </div>
--- apps/kg-admin/components/mk/Matakuliah.tsx
+++ apps/kg-admin/components/mk/Matakuliah.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 export default function Matakuliah() {
   return (
-    <div className="w-[600px] flex flex-col justify-start gap-4">
-      <Image src="/assets/pic-mk.png" alt="pic-mk" width={600} height={600} />
+    <div className="w-[500px] flex flex-col justify-start gap-4">
+      <Image src="/assets/pic-mk.png" alt="pic-mk" width={500} height={500} />
       <div className="py-1 px-2 w-max bg-warning-200 rounded-lg">
-        <p className="font-semibold text-sm">322 Mahasiswa</p>
+        <p className="font-semibold text-xs">322 Mahasiswa</p>
       </div>
-      <h1 className="font-bold text-xl">Manajemen Keuangan</h1>
-      <p className="font-normal">
+      <h1 className="font-bold text-lg">Manajemen Keuangan</h1>
+      <p className="font-sm">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut velit
         repellat cupiditate hic rerum quos nisi enim at similique
         necessitatibus, quia, nostrum officia ducimus, odit deserunt ad
